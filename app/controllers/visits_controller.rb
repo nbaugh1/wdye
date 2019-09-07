34e0d13f2f71b444f3943bd4b2b608413917ee3f
nbaugh1 <nbaugh1@gmail.com>
2019-09-07 12:15:39 -0400

    added views and controllers  for new visit, visits index, edit navbar

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,13 +1,31 @@
 class VisitsController < ApplicationController
+
   def index
+    @visits = Visit.all 
   end
 
   def show
+    @visit = Visit.find(params[:id])
   end
 
   def new
+    @visit = Visit.new
+  end
+
+  def create
+    @visit = Visit.create(visit_params)
+    
+
+    redirect_to visit_path(@visit)
   end
 
   def edit
+    
+  end
+
+  private
+
+  def visit_params
+    params.require(:visit).permit(:visit_date, :description)
   end
 end
